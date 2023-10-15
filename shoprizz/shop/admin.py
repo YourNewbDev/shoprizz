974de8545ad0e5c2b7630ce47e326bb197b272ee
unknown <harvinkrist.dev@gmail.com>
2023-10-15 09:41:35 -0700

    Created Customer Models, might need some fixes later on

--- shoprizz/shop/admin.py
+++ shoprizz/shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . models import Customer, CustomerAddress, Address, Country
 
 # Register your models here.
+admin.site.register(Customer)
+admin.site.register(CustomerAddress)
+admin.site.register(Address)
+admin.site.register(Country)
--- shoprizz/shop/models.py
+++ shoprizz/shop/models.py
@@ -1,3 +1,44 @@
-from django.db import models
+from django.db import models, IntegrityError
+from django.core.validators import MaxValueValidator
+from django.contrib.auth.hashers import make_password
+import random, string
 
 # Create your models here.
+class Customer(models.Model):
+    customer_first_name = models.CharField(max_length=155)
+    customer_last_name = models.CharField(max_length=100)
+    customer_phone_number = models.IntegerField(validators=[MaxValueValidator(99999999999)])
+    customer_username = models.CharField(max_length=10, unique=True)
+    customer_email = models.EmailField(max_length=255, unique=True)
+    customer_password = models.CharField(max_length=128)
+    customer_registered_date = models.DateField(auto_now_add=True)
+
+    # Generate random username
+    def generate_username(self, length=10):
+        characters = string.ascii_letters + string.digits
+        random_username = ''.join(random.choice(characters) for _ in range(length))
+        self.customer_username = random_username
+
+    # Hash password
+    def hash_pass(self, *args, **kwargs):
+        self.customer_password = make_password(self.customer_password)
+        super(Customer, self).save(*args, **kwargs)
+
+class Country(models.Model): # null temporarily while looking for python package
+    country_name = models.CharField(max_length=90, null=True, blank=True)
+
+class Address(models.Model): # null temporarily while looking for python package
+    country_id = models.ForeignKey(Country, on_delete=models.CASCADE)
+    address_region = models.CharField(max_length=155, null=True, blank=True)
+    address_province = models.CharField(max_length=155, null=True, blank=True)
+    address_city = models.CharField(max_length=155, null=True, blank=True)
+    address_postal = models.CharField(max_length=5, null=True, blank=True)
+    address_unit = models.CharField(max_length=10, null=True, blank=True)
+    address_street = models.CharField(max_length=50, null=True, blank=True)
+    address_line1 = models.CharField(max_length=255, null=True, blank=True)
+    address_line2 = models.CharField(max_length=255, null=True, blank=True)
+
+class CustomerAddress(models.Model):
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    address_id = models.ForeignKey(Address, on_delete=models.CASCADE)
+    is_default = models.BooleanField(default=False)
